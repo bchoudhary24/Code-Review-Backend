47e35d22f80a594f9033a004d0b8cae44e5d1faa
Bhumika Choudhary <cbhumika199@gmail.com>
2025-11-11 12:23:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,5 +15,6 @@ app.use(
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(Server is running on port ${port});
+  console.log(`Server is running on port ${port}`);
 });
+
