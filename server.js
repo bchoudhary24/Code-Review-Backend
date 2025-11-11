4e9bf28a3f9a96b86f07748ceee25338b4a15532
Bhumika Choudhary <cbhumika199@gmail.com>
2025-11-11 12:18:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
-require('dotenv').config()
-const app = require('./app')
+const app = require('./app');
+const cors = require('cors');
 
-app.listen(3000, () => {
-    console.log('server is running on http://localhost:3000');
+app.use(
+  cors({
+    origin: [
+      "https://55b9d4d3.code-review-frontend.pages.dev",
+      "https://02dcdf4a.code-review-frontend.pages.dev"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  })
+);
 
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(Server is running on port ${port});
 });
